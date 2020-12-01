0630737a9846e4ac3f4023b8e56244e7293989a0
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-12-01 23:46:42 +0100

    if else / switch

--- index.php
+++ index.php
@@ -1,26 +1,69 @@
 <?php
 
-// Les conditions multiples
 
-	// $pseudo = "Tintin";
-	// $mdp = "Lune";
+// LES CONDITIONS SIMPLES
+
+	$number = 10;
+
+	if ($number == 10) 
+		echo "True";
+	  else 
+		echo "False";
+	
 
 
 
-	// if ($pseudo == "Tintin" AND $mdp == "Lune") {
 
-	// 	echo "Mot de passe valide.";
+// SWITCH
+// Uniquement pour tester des égalités
 
+
+	// $note = 20;
+
+	// switch ($note) {
+	// 	case 1:
+	// 		echo "T'es nul.";
+	// 		break;
+	// 	case 1:
+	// 	echo "T'es nul.";
+	// 		break;
+
+	// 	default:
+	// 		echo "C'est bien !";
+	// 		break;
 	// }
 
-// Les conditions ternaires
 
-// Number % 10 == 0
 
-	$nombre = 20;
 
-	echo ($nombre % 10 == 0)? 'true' : 'false';
 
+
+
+
+
+
+// IF ELSE IF else
+
+	// $age = "18";
+	
+	// if ($age < 18) {
+
+	// 	echo "Vos êtes mineur";
+
+	// } elseif($age == 18) {
+
+	// 	echo "Vous êtes enfin majeur!";
+		
+	// } elseif ($age > 18) {
+		
+	// 	echo "Vous êtes majeur.";
+
+	// } else {
+
+	// 	echo "Erreur";
+
+	// }
+	
 
 
 
