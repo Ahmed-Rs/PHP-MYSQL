3a2a089ffb2bb5a54bf09b2d43808a9d4e3ff8b4
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-12-19 23:40:32 +0100

    Exercice Lire Ecrire dans un fichier

--- index.php
+++ index.php
@@ -8,19 +8,35 @@
 // a - Lire le fichier et le créer s'il n'existe pas
 // a + - Lire et écrire dans un fichier et le créer s'il n'existe pas et écrire à la suite du fichier 
 
-// OUVRIR
-$fichier = fopen('utilisateur.txt', 'r+'); // Lr r+, contrairement au a+, remplace le texte déjà existant au lieu de s'ajouter à lui
+// COMPTEUR DE VISITE
+// compteur.txt
+// 0;
+// Incrémenter cette valeur à chaque fois qu'un utilisateur visite notre et lui afficher son numéro de visiteur
+// Vous êtes la x-ème visite sur ce site
 
-// ECRIRE
-// fputs($fichier, 'Non');
+// OUVRIR
+$fichier = fopen('compteur.txt', 'r+');
 
 // LIRE
-// fgets();  - lit une ligne
-// fgetc();  - lit un caractère
+$pages = fgets($fichier);
 
-$ligne = fgets($fichier);
+// CURSEUR A METTRE A GAUCHE, AU DEBUT DU TEXTE, SINON IL ECRIRA A LA SUITE DU CHIFFRE DEJA PRESENT. Le cuseur a en effet été déplacé vers la fin de la ligne en LISANT (fgets) avant d'ECRIRE (fputs)
+// Dans la version précédente , ce n'était pas le cas car on ECRIVAIT avant de LIRE.
+// Le fait de LIRE avant d'ECRIRE a donc le même effet que le a+
+fseek($fichier, 0); // On place le curseur à l'index 0 des chars dans notre fichier
 
-echo $ligne;
+$pages += 1;
+
+// ECRIRE
+fputs($fichier, $pages);
 
 // FERMER
 fclose($fichier);
+
+echo 'Bonjour, vous le '.$pages.' ème visiteur du site !';
+
+
+
+
+
+// fclose($fichier);
