8507a6260eb00e8ac7d78133e840d79c4cd5f0f4
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-12-19 17:21:31 +0100

    Lire et écrire dans un fichier

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
-
 <?php
 
-?>
+// LIRE / ECRIRE DANS UN FICHIER
 
+// MODE
+// r - Lire le fichier
+// r + - Lire et écrire dans un fichier
+// a - Lire le fichier et le créer s'il n'existe pas
+// a + - Lire et écrire dans un fichier et le créer s'il n'existe pas et écrire à la suite du fichier 
 
-<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="utf-8">
-			<title>PHP</title>
-		</head>
-		<body>
+// OUVRIR
+$fichier = fopen('utilisateur.txt', 'r+'); // Lr r+, contrairement au a+, remplace le texte déjà existant au lieu de s'ajouter à lui
 
-		</body>
-	</html>
+// ECRIRE
+// fputs($fichier, 'Non');
 
+// LIRE
+// fgets();  - lit une ligne
+// fgetc();  - lit un caractère
 
+$ligne = fgets($fichier);
+
+echo $ligne;
+
+// FERMER
+fclose($fichier);
